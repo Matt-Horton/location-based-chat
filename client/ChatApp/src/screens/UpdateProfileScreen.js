3d233699c2862b7ae64ffe025a090bce12888c41
Matt Horton <matt-horton@Matts-MBP.lan>
2020-03-08 19:22:10 +0000

    Added image picker library & displayed selected profile picture

--- client/ChatApp/src/screens/UpdateProfileScreen.js
+++ client/ChatApp/src/screens/UpdateProfileScreen.js
@@ -1,14 +1,16 @@
-import React, {useState, useContext} from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, StyleSheet, Image } from 'react-native';
 import FormButton from '../components/FormButton';
 import FormInput from '../components/FormInput';
-import {Context as UserInfoContext} from '../context/UserInfoContext';
-import {updateUserProfile} from '../utils/auth';
+import { Context as UserInfoContext } from '../context/UserInfoContext';
+import { updateUserProfile } from '../utils/auth';
+import ImagePicker from 'react-native-image-picker';
 
 const UpdateProfileScreen = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [bio, setBio] = useState('');
+  const [photo, setPhoto] = useState('');
   const { state } = useContext(UserInfoContext);
 
   const submitUpdateProfile = () => {
@@ -19,11 +21,32 @@ const UpdateProfileScreen = () => {
     };
 
     updateUserProfile(state.userId, userDetails, state.authToken);
-  }
+  };
+
+  const selectProfilePicture = () => {
+
+    ImagePicker.showImagePicker(response => {
+      console.log(response.path);
+      if (response.uri) {
+
+        let source = { uri: response.uri };
+        console.log(response);
+        setPhoto(source);
+      }
+    })
+  };
 
   return (
     <View style={styles.container} >
-      <FormInput 
+      <Image
+        source={{ uri: photo.uri }}
+        style={{ width: 100, height: 100, borderColor: '#000', borderWidth: 1 }}
+      />
+      <FormButton
+        text="Choose Profile Picture"
+        submitForm={selectProfilePicture}
+      />
+      <FormInput
         placeholder='First Name'
         value={firstName}
         updateValue={setFirstName}
@@ -38,7 +61,7 @@ const UpdateProfileScreen = () => {
         value={bio}
         updateValue={setBio}
       />
-      <FormButton 
+      <FormButton
         text="Submit"
         submitForm={submitUpdateProfile}
       />
